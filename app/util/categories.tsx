559d568159695f8aad666d94b402ffc803a81d94
CoffeeAddict25 <ahnsanou.ahnoux@gmail.com>
2025-10-09 17:56:24 +0200

    updated the categories logic

--- app/util/categories.tsx
+++ app/util/categories.tsx
@@ -1,20 +1,3 @@
-// const categories = [
-//   { id: 1, name: "Grains" },
-//   { id: 2, name: "Baked goods" },
-//   { id: 3, name: "Alcohol" },
-//   { id: 4, name: "Dairy" },
-//   { id: 5, name: "Desserts" },
-//   { id: 6, name: "Seafood" },
-//   { id: 7, name: "Beverages" },
-//   { id: 8, name: "Hot drinks" },
-//   { id: 9, name: "Fruits and Veg" },
-//   { id: 10, name: "Meat" },
-//   { id: 11, name: "Personal hygiene" },
-//   { id: 12, name: "Snacks and treats" },
-//   { id: 13, name: "Spices and herbs" },
-//   { id: 15, name: "Cleaning Supplies" },
-// ];
-
 const categoriesArr = [
   "Grains",
   "Baked goods",
