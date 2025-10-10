a7a520608a1a757d37744e744d7deacf802a76b4
CoffeeAddict25 <ahnsanou.ahnoux@gmail.com>
2025-10-10 14:26:47 +0200

    fixing pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,11 @@ import "./globals.css";
 import Link from "next/link";
 import Navbar from "./components/Navbar";
 import { ItemProvider } from "./context/appContext";
-import Head from "next/head";
 
 export const metadata = {
   title: "Genesis",
   description: "an offline shopping list manager app",
-  manifest: "./manifest.json",
+  manifest: "/manifest.json",
   icons: {
     // Primary favicon
     icon: [
@@ -38,10 +37,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#ffb343" />
-      </Head>
       <body className="w-svw h-svh m-0">
         <ItemProvider>
           <div className="flex items-center justify-between mb-10 sticky inset-0 z-20 bg-background-white ">
